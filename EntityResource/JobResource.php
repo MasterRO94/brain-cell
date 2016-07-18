42ed43fe5bad20f0fa4c4a821b9125a92a0bbd0e
Matthew Rose <matthew.rose@printed.com>
2016-07-18 12:14:05 +0100

    Jobs API returns production houses and shops.

--- EntityResource/JobResource.php
+++ EntityResource/JobResource.php
@@ -16,11 +16,70 @@ class JobResource extends AbstractResource
     protected $id;
 
     /**
+     * @var ProductionHouseResource
+     */
+    protected $productionHouse;
+
+    /**
+     * @var ShopResource
+     */
+    protected $shop;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAssociatedResources()
+    {
+        return [
+            'productionHouse' => ProductionHouseResource::class,
+            'shop' => ShopResource::class
+        ];
+    }
+
+    /**
      * @return int
      */
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * @return null|ProductionHouseResource
+     */
+    public function getProductionHouse()
+    {
+        return $this->productionHouse;
+    }
+
+    /**
+     * @param ProductionHouseResource $productionHouse
+     *
+     * @return $this
+     */
+    public function setProductionHouse(ProductionHouseResource $productionHouse)
+    {
+        $this->productionHouse = $productionHouse;
+        return $this;
+    }
+
+    /**
+     * @return ShopResource
+     */
+    public function getShop()
+    {
+        return $this->shop;
+    }
+
+    /**
+     * @param ShopResource $shop
+     *
+     * @return $this
+     */
+    public function setShop(ShopResource $shop)
+    {
+        $this->shop = $shop;
+        return $this;
     }
 
 }
