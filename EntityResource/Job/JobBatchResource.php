9433f69d126023ce80ac947d7ae3a6d1b852b2fc
Alex Moon <alex.moon@printed.com>
2017-06-14 13:03:03 +0100

    BCL-34 - move delivery to job batch

--- EntityResource/Job/JobBatchResource.php
+++ EntityResource/Job/JobBatchResource.php
@@ -5,6 +5,7 @@
 
 namespace Brain\Cell\EntityResource\Job;
 
+use Brain\Cell\EntityResource\Delivery\DeliveryResource;
 use Brain\Cell\Transfer\AbstractResource;
 
 class JobBatchResource extends AbstractResource
@@ -16,10 +17,46 @@ class JobBatchResource extends AbstractResource
     protected $id;
 
     /**
+     * @var DeliveryResource
+     *
+     * @Assert\Valid()
+     * @Assert\NotBlank()
+     */
+    protected $delivery;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAssociatedResources()
+    {
+        return [
+            'delivery' => DeliveryResource::class,
+        ];
+    }
+    /**
      * @return string
      */
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * @return DeliveryResource
+     */
+    public function getDelivery()
+    {
+        return $this->delivery;
+    }
+
+    /**
+     * @param DeliveryResource $delivery
+     *
+     * @return JobBatchResource
+     */
+    public function setDelivery(DeliveryResource $delivery)
+    {
+        $this->delivery = $delivery;
+        return $this;
     }
 }
