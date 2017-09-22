8b3dc41527721b6233d3db9ae8ccac32093c5e4d
Matthew Rose <matthew.rose@printed.com>
2017-09-22 16:25:43 +0100

    Added artwork statuses to the `ArtworkStatusResource`

--- EntityResource/Artwork/ArtworkStatusResource.php
+++ EntityResource/Artwork/ArtworkStatusResource.php
@@ -9,4 +9,11 @@ use Brain\Cell\EntityResource\Common\AbstractStatusResource;
  */
 class ArtworkStatusResource extends AbstractStatusResource
 {
+    const STATUS_NEW = 'artwork.status.new';
+    const STATUS_VERIFIED = 'artwork.status.verified';
+    const STATUS_PENDING_DOWNLOAD = 'artwork.status.pending_download';
+    const STATUS_PENDING_VALIDATION = 'artwork.status.pending_validation';
+    const STATUS_PENDING_UPLOAD = 'artwork.status.pending_upload';
+    const STATUS_FAILED_DOWNLOAD = 'artwork.status.failed_download';
+    const STATUS_INVALID_MIME_TYPE = 'artwork.status.invalid_mime_type';
 }
