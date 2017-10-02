9910cbcac0dabf395010bb88773566eacd1668d1
Liam Miller <liam.miller@printed.com>
2017-10-02 11:54:07 +0100

    PALM-296 - I have created a client resource and made the shop and prodction house resources extend from this class, same as how the entities are represented in brain.

---
+++ EntityResource/ClientResource.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Brain\Cell\EntityResource;
+
+use Brain\Cell\Transfer\AbstractResource;
+
+class ClientResource extends AbstractResource
+{
+    /**
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @var AddressResource
+     */
+    protected $address;
+
+    /**
+     * @var string
+     */
+    protected $phone;
+
+    /**
+     * @var string
+     */
+    protected $email;
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAssociatedResources()
+    {
+        return [
+            'address' => AddressResource::class,
+        ];
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+    }
+
+    /**
+     * @return AddressResource
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+
+    }
+}
--- EntityResource/ShopResource.php
+++ EntityResource/ShopResource.php
@@ -2,97 +2,10 @@
 
 namespace Brain\Cell\EntityResource;
 
-use Brain\Cell\Logical\EmailAwareTrait;
-use Brain\Cell\Transfer\AbstractResource;
 
 /**
  * {@inheritdoc}
  */
-class ShopResource extends AbstractResource
+class ShopResource extends ClientResource
 {
-    /**
-     * @var int
-     */
-    protected $id;
-
-    /**
-     * @var string
-     */
-    protected $name;
-
-    /**
-     * @var AddressResource
-     */
-    protected $address;
-
-    /**
-     * @var string
-     */
-    protected $phone;
-
-    /**
-     * @var string
-     */
-    protected $email;
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getAssociatedResources()
-    {
-        return [
-            'address' => AddressResource::class,
-        ];
-    }
-
-    /**
-     * @return int
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * @return string
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    /**
-     * @param string $name
-     * @return OptionResource
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-        return $this;
-    }
-
-    /**
-     * @return AddressResource
-     */
-    public function getAddress()
-    {
-        return $this->address;
-    }
-
-    /**
-     * @return string
-     */
-    public function getPhone()
-    {
-        return $this->phone;
-    }
-
-    /**
-     * @return string
-     */
-    public function getEmail()
-    {
-        return $this->email;
-
-    }
 }
