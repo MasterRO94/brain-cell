92085685b65379aeb8a3d9e86ca3ca80c265f03e
Alex Moon <alex.moon@printed.com>
2017-12-06 12:32:45 +0000

    EYES-113 and PALM-369 - thumbnails in Cell :)

--- src/Brain/Cell/Client/Delegate/ArtworkDelegateClient.php
+++ src/Brain/Cell/Client/Delegate/ArtworkDelegateClient.php
@@ -20,4 +20,28 @@ class ArtworkDelegateClient extends DelegateClient
 
         return $this->stream($context);
     }
+
+    /**
+     * @param string $id
+     *
+     * @param int $pageNumber
+     * @param int|null $width
+     * @param int|null $height
+     * @return StreamInterface
+     */
+    public function downloadPreview($id, $pageNumber, $width = null, $height = null)
+    {
+        $context = $this->configuration->createRequestContext();
+        if ($width) {
+            if ($height) {
+                $url = sprintf('/artworks/%s/preview/%s/%s/%s', $id, $pageNumber, $width, $height);
+            } else {
+                $url = sprintf('/artworks/%s/preview/%s/%s', $id, $pageNumber, $width);
+            }
+        } else {
+            $url = sprintf('/artworks/%s/preview/%s', $id, $pageNumber);
+        }
+        $context->prepareContextForGet($url);
+        return $this->stream($context);
+    }
 }
