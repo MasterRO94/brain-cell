4f4bd1372360b9272246be61df2aa9b2a746d8ad
Alex Moon <alex.moon@printed.com>
2018-02-27 13:34:57 +0000

    PALM-432 - job meta resource

--- src/Brain/Cell/EntityResource/Job/JobMetaResource.php
+++ src/Brain/Cell/EntityResource/Job/JobMetaResource.php
@@ -7,6 +7,21 @@ use Brain\Cell\Transfer\AbstractResource;
 class JobMetaResource extends AbstractResource
 {
     /**
+     * @var array $data
+     */
+    protected $data;
+
+    /**
+     * @var string $group
+     */
+    protected $group;
+
+    /**
+     * @var string $note
+     */
+    protected $note;
+
+    /**
      * {@inheritdoc}
      */
     public function getUnstructuredFields()
@@ -16,5 +31,52 @@ class JobMetaResource extends AbstractResource
         ];
     }
 
-    protected $data;
+    /**
+     * @return array
+     */
+    public function getData(): ?array
+    {
+        return $this->data;
+    }
+
+    /**
+     * @param array $data
+     */
+    public function setData(array $data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGroup(): ?string
+    {
+        return $this->group;
+    }
+
+    /**
+     * @param string $group
+     */
+    public function setGroup(string $group)
+    {
+        $this->group = $group;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNote(): ?string
+    {
+        return $this->note;
+    }
+
+    /**
+     * @param string $note
+     */
+    public function setNote(string $note)
+    {
+        $this->note = $note;
+    }
+
 }
