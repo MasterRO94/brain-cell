2ae9bf8569ef2a19fad2adf945cb69b6877b3b6e
Alex Moon <alex.moon@printed.com>
2018-04-12 13:34:11 +0100

    PALM-379 - add constants for canonicals

--- src/Brain/Cell/EntityResource/Artwork/ArtworkIssueResource.php
+++ src/Brain/Cell/EntityResource/Artwork/ArtworkIssueResource.php
@@ -6,4 +6,11 @@ use Brain\Cell\EntityResource\AbstractNoteResource;
 
 class ArtworkIssueResource extends AbstractNoteResource
 {
+    const ARTWORK_ISSUE_PREFLIGHT = 'artwork_issue.validation.preflight';
+    const ARTWORK_ISSUE_DOCUMENT_ENCRYPTED = 'artwork_issue.validation.document_encrypted';
+    const ARTWORK_ISSUE_PAGE_COUNT_INVALID = 'artwork_issue.validation.page_count_invalid';
+    const ARTWORK_ISSUE_PAGE_TRIM_BOX_MISSING = 'artwork_issue.validation.page_trim_box_missing';
+    const ARTWORK_ISSUE_PAGE_TRIM_BOX_PLACEMENT = 'artwork_issue.validation.page_trim_box_placement';
+    const ARTWORK_ISSUE_PAGE_MEDIA_BOX_MISSING = 'artwork_issue.validation.page_media_box_missing';
+    const ARTWORK_ISSUE_PAGE_BLEED_BOX_PLACEMENT = 'artwork_issue.validation.page_bleed_box_placement';
 }
