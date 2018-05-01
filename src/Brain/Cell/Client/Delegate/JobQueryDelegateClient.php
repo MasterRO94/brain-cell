c09d06dd72651651985510720714e00e14e8619b
Alex Moon <alex.moon@printed.com>
2018-05-01 15:18:40 +0100

    BRN-617 - add job query note canonicals and job query note post

--- src/Brain/Cell/Client/Delegate/JobQueryDelegateClient.php
+++ src/Brain/Cell/Client/Delegate/JobQueryDelegateClient.php
@@ -3,6 +3,7 @@
 namespace Brain\Cell\Client\Delegate;
 
 use Brain\Cell\Client\DelegateClient;
+use Brain\Cell\EntityResource\Job\JobQueryNoteResource;
 use Brain\Cell\EntityResource\Job\JobQueryResource;
 
 class JobQueryDelegateClient extends DelegateClient
@@ -12,11 +13,52 @@ class JobQueryDelegateClient extends DelegateClient
      *
      * @return JobQueryResource
      */
-    public function getQuery($id)
+    public function getJobQuery($id)
     {
         $context = $this->configuration->createRequestContext();
         $context->prepareContextForGet(sprintf('/queries/%s', $id));
 
         return $this->request($context, new JobQueryResource());
     }
+
+    /**
+     * @param string $jobId
+     * @param JobQueryResource $resource
+     *
+     * @return JobQueryResource
+     */
+    public function postJobQuery(string $jobId, JobQueryResource $resource)
+    {
+        $context = $this->configuration->createRequestContext();
+        $context->prepareContextForPost(sprintf(
+            '/jobs/%s/queries',
+            $jobId
+        ));
+
+        $handler = $this->configuration->getResourceHandler();
+        $context->setPayload($handler->serialise($resource));
+
+        return $this->request($context, $resource);
+    }
+
+    /**
+     * @param string $queryId
+     * @param JobQueryNoteResource $resource
+     *
+     * @return JobQueryNoteResource
+     */
+    public function postJobQueryNote(string $queryId, JobQueryNoteResource $resource)
+    {
+        $context = $this->configuration->createRequestContext();
+        $context->prepareContextForPost(sprintf(
+            '/queries/%s/notes',
+            $queryId
+        ));
+
+        $handler = $this->configuration->getResourceHandler();
+        $context->setPayload($handler->serialise($resource));
+
+        return $this->request($context, $resource);
+    }
+
 }
--- src/Brain/Cell/EntityResource/Job/JobQueryNoteResource.php
+++ src/Brain/Cell/EntityResource/Job/JobQueryNoteResource.php
@@ -6,5 +6,7 @@ use Brain\Cell\EntityResource\AbstractNoteResource;
 
 class JobQueryNoteResource extends AbstractNoteResource
 {
-
+    const CANONICAL_GENERIC = 'job_query_note.canonical.generic';
+    const CANONICAL_CREATION = 'job_query_note.canonical.creation';
+    const CANONICAL_RESOLUTION = 'job_query_note.canonical.resolution';
 }
