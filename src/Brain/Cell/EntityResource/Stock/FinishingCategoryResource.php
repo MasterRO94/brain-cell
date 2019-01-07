f8f9dc2a4c30037149cb81ccaf7dc1605ecea2b1
Matthew Rose <matthew.rose@printed.com>
2019-01-07 12:04:30 +0000

    Prevent breaking change.

---
+++ src/Brain/Cell/EntityResource/Stock/FinishingCategoryResource.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Brain\Cell\EntityResource\Stock;
+
+use Brain\Cell\EntityResource\Stock\Finishing\FinishingCategoryResource as Proper;
+
+/**
+ * @deprecated Use the one in the finishing namespace.
+ */
+class FinishingCategoryResource extends Proper
+{
+
+}
---
+++ src/Brain/Cell/EntityResource/Stock/FinishingItemResource.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Brain\Cell\EntityResource\Stock;
+
+use Brain\Cell\EntityResource\Stock\Finishing\FinishingItemResource as Proper;
+
+/**
+ * @deprecated Use the one in the finishing namespace.
+ */
+class FinishingItemResource extends Proper
+{
+
+}
