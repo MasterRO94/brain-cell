07360499d02c3114980aa50f27e49be8acb2d2aa
Alex Moon <alex.moon@printed.com>
2019-03-12 16:23:41 +0000

    fix

---
+++ src/Brain/Cell/EntityResource/Stock/Finishing/FinishingItemResource.php
@@ -0,0 +1,134 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brain\Cell\EntityResource\Stock\Finishing;
+
+use Brain\Cell\EntityResource\Prototype\ResourceAliasTrait;
+use Brain\Cell\EntityResource\Prototype\ResourceIdentityTrait;
+use Brain\Cell\Transfer\AbstractResource;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * {@inheritdoc}
+ */
+final class FinishingItemResource extends AbstractResource implements
+    FinishingItemResourceInterface
+{
+    use ResourceIdentityTrait;
+    use ResourceAliasTrait;
+
+    /**
+     * @var string
+     *
+     * @Assert\NotBlank()
+     */
+    protected $alias;
+
+    /** @var string */
+    protected $name;
+
+    /** @var int */
+    protected $weight;
+
+    /** @var string */
+    protected $weightUnit;
+
+    /** @var bool */
+    protected $isDefault = false;
+
+    /** @var bool */
+    protected $configurable;
+
+    /** @var mixed[] */
+    protected $configuration;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getUnstructuredFields(): array
+    {
+        return [
+            'configuration',
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the human-readable name.
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isDefault(): bool
+    {
+        return $this->isDefault;
+    }
+
+    public function setIsDefault(bool $isDefault): void
+    {
+        $this->isDefault = $isDefault;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isConfigurable(): bool
+    {
+        return $this->configurable;
+    }
+
+    public function setConfigurable(bool $configurable): void
+    {
+        $this->configurable = $configurable;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfiguration(): array
+    {
+        return $this->configuration;
+    }
+
+    /**
+     * @param mixed[] $configuration
+     */
+    public function setConfiguration(array $configuration): void
+    {
+        $this->configuration = $configuration;
+    }
+
+    public function getWeight(): int
+    {
+        return $this->weight;
+    }
+
+    public function setWeight(int $weight): self
+    {
+        $this->weight = $weight;
+    }
+
+    public function getWeightUnit(): string
+    {
+        return $this->weightUnit;
+    }
+
+    public function setWeightUnit(string $weightUnit): self
+    {
+        $this->weightUnit = $weightUnit;
+    }
+}
