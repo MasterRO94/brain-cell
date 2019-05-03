6b44411abcad06ec1a0d60a290a9bdfa22262d1b
Alex Moon <alex.moon@printed.com>
2019-05-03 13:31:36 +0100

    BRN-950 - get job component

--- src/Brain/Cell/Client/Delegate/Job/JobComponentDelegateClient.php
+++ src/Brain/Cell/Client/Delegate/Job/JobComponentDelegateClient.php
@@ -6,11 +6,30 @@ namespace Brain\Cell\Client\Delegate\Job;
 
 use Brain\Cell\Client\DelegateClient;
 use Brain\Cell\EntityResource\Artwork\ArtworkResource;
+use Brain\Cell\EntityResource\Job\JobComponentResource;
 use Brain\Cell\EntityResource\Job\JobComponentResourceInterface;
 use Brain\Cell\EntityResource\Job\JobResourceInterface;
 
 class JobComponentDelegateClient extends DelegateClient
 {
+    public function get(
+        string $jobId,
+        string $jobComponentId
+    ): JobComponentResource {
+        $context = $this->configuration->createRequestContext();
+
+        $context->prepareContextForGet(sprintf(
+            '/jobs/%s/components/%s',
+            $jobId,
+            $jobComponentId
+        ));
+
+        /** @var JobResource $resource */
+        $resource = $this->request($context, new JobComponentResource());
+
+        return $resource;
+    }
+
     public function updateArtwork(
         JobResourceInterface $job,
         JobComponentResourceInterface $component,
@@ -20,7 +39,11 @@ class JobComponentDelegateClient extends DelegateClient
         $payload = $handler->serialise($artwork);
 
         $context = $this->configuration->createRequestContext();
-        $context->prepareContextForPut(sprintf('/jobs/%s/components/%s/artwork', $job->getId(), $component->getId()));
+        $context->prepareContextForPut(sprintf(
+            '/jobs/%s/components/%s/artwork',
+            $job->getId(),
+            $component->getId()
+        ));
         $context->setPayload($payload);
 
         /** @var JobComponentResourceInterface $resource */
