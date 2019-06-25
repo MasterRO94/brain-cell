a3fed21104383276d045db4265231b342752691b
Ashley Snowdon <ashley.snowdon@printed.com>
2019-06-25 15:06:45 +0100

    Updated to use Interface for mocking

--- src/Brain/Cell/EntityResource/Job/JobResource.php
+++ src/Brain/Cell/EntityResource/Job/JobResource.php
@@ -8,7 +8,6 @@ use Brain\Cell\EntityResource\Artifact\ArtifactResource;
 use Brain\Cell\EntityResource\Artwork\ArtworkResource;
 use Brain\Cell\EntityResource\Common\DateResource;
 use Brain\Cell\EntityResource\Job\ClientWorkflow\PhaseResource;
-use Brain\Cell\EntityResource\Pricing\PriceResource;
 use Brain\Cell\EntityResource\Product\ProductResource;
 use Brain\Cell\EntityResource\Product\ProductResourceInterface;
 use Brain\Cell\EntityResource\Prototype\ResourceIdentityTrait;
@@ -146,7 +145,6 @@ class JobResource extends AbstractResource implements
             'batch' => JobBatchResource::class,
             'artifact' => ArtifactResource::class,
             'dimensions' => ThreeDimensionalResource::class,
-            'price' => PriceResource::class,
             'status' => JobStatusResource::class,
             'clonedFrom' => self::class,
             'meta' => JobMetaResource::class,
