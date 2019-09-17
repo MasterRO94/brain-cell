d2ce4e48f5ac74967e43c5f813cb2b0beeee117f
Alex Moon <alex.moon@printed.com>
2019-09-17 11:06:50 +0100

    PALM-261 - add imports

--- src/Brain/Cell/BrainClientInterface.php
+++ src/Brain/Cell/BrainClientInterface.php
@@ -4,6 +4,22 @@ declare(strict_types=1);
 
 namespace Brain\Cell;
 
+use Brain\Cell\Client\Delegate\ArtifactDelegateClient;
+use Brain\Cell\Client\Delegate\ArtworkDelegateClient;
+use Brain\Cell\Client\Delegate\AuthenticationDelegateClient;
+use Brain\Cell\Client\Delegate\ClientWorkflowDelegateClient;
+use Brain\Cell\Client\Delegate\DeliveryDelegateClient;
+use Brain\Cell\Client\Delegate\File\FileDelegateClient;
+use Brain\Cell\Client\Delegate\Job\JobBatchDelegateClient;
+use Brain\Cell\Client\Delegate\Job\JobComponentDelegateClient;
+use Brain\Cell\Client\Delegate\Job\JobDelegateClient;
+use Brain\Cell\Client\Delegate\Job\JobQueryDelegateClient;
+use Brain\Cell\Client\Delegate\Pricing\PricingDelegateClient;
+use Brain\Cell\Client\Delegate\ProductDelegateClient;
+use Brain\Cell\Client\Delegate\Production\ProductionDelegateClient;
+use Brain\Cell\Client\Delegate\StockDelegateClient;
+use Brain\Cell\Client\Delegate\WebhookDelegateClient;
+
 interface BrainClientInterface
 {
     public function authentication(): AuthenticationDelegateClient;
